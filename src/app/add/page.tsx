c575d285570e6d8887ee9f484a4d8f651be85aa7
Will <dnam310199@gmail.com>
2024-07-29 22:49:58 -0700

    fixing deploy

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { createPost } from "@/actions/actions";
+// import { createPost } from "@/actions/actions";
 
 export default function AddPage() {
   return (
     <>
       <div>
-        <form action={createPost} className="flex flex-col items-center gap-4">
+        <form className="flex flex-col items-center gap-4">
           <input type="text" name="title" id="title" placeholder="Title" />
           <button type="submit">Submit</button>
         </form>
